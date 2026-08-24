80e71f9dd91fc1645b1f76618428aa579c796a38
Pedro Bezerra <dulce.leia06@gmail.com>
2026-08-24 12:34:33 -0300

    att

--- pratica1-Vite/my-app/src/App.tsx
+++ pratica1-Vite/my-app/src/App.tsx
@@ -1,7 +1,18 @@
-function App() {
+import Tarefa from "./components/Tarefa"
 
+function App() {
+  const tarefas = [
+    {id: 1, texto: "Estudar React"},
+    {id: 2, texto: "Fazer compras"},
+    {id: 3, texto: "Responder e-mails"}
+  ];
   return (
-    <h1>Olá React!</h1>
+    <main>
+      <h1>To-Do List App</h1>
+      <ul>
+        {tarefas.map(tarefa => <Tarefa key={tarefa.id} texto={tarefa.texto}/>)}
+      </ul>
+    </main>
   )
 }
 
