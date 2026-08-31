ff5f337641d2994f739cad36784a1df6297d0731
Pedro Bezerra <dulce.leia06@gmail.com>
2026-08-31 10:02:32 -0300

    att

--- pratica1-Vite/my-app/src/App.tsx
+++ pratica1-Vite/my-app/src/App.tsx
@@ -1,14 +1,37 @@
+import { useState } from "react";
 import Tarefa from "./components/Tarefa"
 
 function App() {
-  const tarefas = [
-    {id: 1, texto: "Estudar React"},
-    {id: 2, texto: "Fazer compras"},
-    {id: 3, texto: "Responder e-mails"}
-  ];
+
+  const [tarefas, setTarefas] = useState([
+    {id: 1, texto: "Estudar React"}
+  ]);
+  const [novaTarefa, setNovaTarefa] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(novaTarefa.trim() === '') return;
+
+    const novoid = tarefas[tarefas.length - 1].id +1;
+    const nova = {
+      id: novoid,
+      texto: novaTarefa.trim()
+    }
+    setTarefas([...tarefas, nova]);
+    setNovaTarefa('');
+  }
+
   return (
     <main>
       <h1>To-Do List App</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Digite uma nova tarefa" 
+        value={novaTarefa}
+        onChange={(e) => setNovaTarefa(e.target.value)}
+        />
+        <button type="submit">Adicionar</button>
+      </form>
       <ul>
         {tarefas.map(tarefa => <Tarefa key={tarefa.id} texto={tarefa.texto}/>)}
       </ul>
